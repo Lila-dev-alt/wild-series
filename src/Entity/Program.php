6592964543a2da39bbfa704de8899c9890d7301d
Elisa BOURG <elisabourg@macbook-air-de-elisa-1.home>
2020-05-28 22:55:22 +0200

    quete 13

--- src/Entity/Program.php
+++ src/Entity/Program.php
@@ -6,9 +6,12 @@ use App\Repository\ProgramRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass=ProgramRepository::class)
+ * @UniqueEntity(fields={"title"}, message="Ce programme existe déjà !")
  */
 class Program
 {
@@ -21,11 +24,18 @@ class Program
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="Ajouter un programme")
+     * @Assert\Length(max="255", maxMessage="La catégorie saisie {{ value }} est trop longue, elle ne devrait pas dépasser {{ limit }} caractères")
      */
     private $title;
 
     /**
      * @ORM\Column(type="text")
+     * @Assert\NotBlank(message="Ajouter un synopsis")
+     *  @Assert\Regex(
+     *    pattern="/(plus belle la vie)/",
+     *     match=false,
+     *     message="On parle vraies séries ici")
      */
     private $summary;
 
